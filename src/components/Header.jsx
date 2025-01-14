e5bf0836511518a7acb3ff483a8cf7004353a539
Eduardo-Alg <eduardoalg.97@gmail.com>
2025-01-14 09:13:49 -0300

    Feliz-Ano-Novo-2025

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+export default function Header(){
+    return(
+        <header>
+            <img src="" alt="" />
+            <nav>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
