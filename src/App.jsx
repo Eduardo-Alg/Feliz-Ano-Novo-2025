b86c1afcddb2c41e5ea77a604b31b7b53c3208d4
Eduardo-Alg <eduardoalg.97@gmail.com>
2025-01-14 10:19:22 -0300

    Projeto Ano-Novo

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from "./Components/Header"
+import Main from "./Components/Main"
+import Footer from "./Components/Footer"
 import './GlobalStyle.scss'
 
 export default function App(){
   return(
     <>
-    <Header/>
-    <Main/>
-    <Footer/>
+   <Header/>
+   <Main/>
+   <Footer/>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
+import logo from '../assets/logo.png'
+import S from './style.module.scss'
 
 export default function Header(){
     return(
         <header>
-            <img src="" alt="" />
-            <nav>
+            <img src={logo} alt="imagem da cidra de ano novo" />
+            <nav className={S.boxMenu}>
                 <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    <li>Paz</li>
+                    <li>Saúde</li>
+                    <li>Alegria</li>
                 </ul>
             </nav>
         </header>
